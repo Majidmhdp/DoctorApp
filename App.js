c836bdf2665e1224aa2447f95abab56301c253c8
Majid <majid.mhdp@gmail.com>
2020-12-17 13:35:37 +0330

    fix

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { createStackNavigator } from '@react-navigation/stack';
-import { StyleSheet, Text, View, Platform, TouchableOpacity, Screen } from "react-native";
+import { StyleSheet, Text, View, Platform, TouchableOpacity, Screen, SafeAreaView, Image } from "react-native";
 import i18next from './i18n';
 import { useTranslation } from 'react-i18next';
 import { Router, Route, Link } from "./react-router";
@@ -11,115 +11,82 @@ import * as Font from 'expo-font';
 import { render } from "react-dom";
 import Home from './Home';
 import About from './About';
+import Icon from 'react-native-vector-icons/Ionicons';
+import {
+  DrawerContentScrollView,
+  DrawerItemList,
+  DrawerItem,
+  DrawerItems,
+} from '@react-navigation/drawer';
 
-  //default start up language
-  i18next.changeLanguage('fa');
-  
-  Font.loadAsync({
-    'Yekan': require('./assets/fonts/iryekanregular.ttf'),
-  });
-  const Drawer = createDrawerNavigator();
+//default start up language
+i18next.changeLanguage('fa');
 
-  export default function App() {
+Font.loadAsync({
+  'Yekan': require('./assets/fonts/iryekanregular.ttf'),
+});
+const Drawer = createDrawerNavigator();
+const Stack = createStackNavigator();
+
+function CustomDrawerContent(props) {
+  return (
+    <DrawerContentScrollView {...props}>
+    <LogoTitle />
+    <DrawerItem
+         label="test"
+         onPress={() => Linking.openURL('https://mywebsite.com/help')}
+         style={{ alignSelf: 'flex-end' }}
+       />
+      <DrawerItemList {...props} />
     
+       <DrawerItem
+         label="Help"
+         headerTitle="true"
+         headerTitleStyle="true"
+         onPress={() => Linking.openURL('https://mywebsite.com/help')}
+       />
+     </DrawerContentScrollView>
+  );
+}
+
+function LogoTitle() {
+  return (
+<Image
+      style={{ width: 50, height: 50, alignSelf: 'center' }}
+      source={{
+        uri:
+          'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADMAAAAzCAYAAAA6oTAqAAAAEXRFWHRTb2Z0d2FyZQBwbmdjcnVzaEB1SfMAAABQSURBVGje7dSxCQBACARB+2/ab8BEeQNhFi6WSYzYLYudDQYGBgYGBgYGBgYGBgYGBgZmcvDqYGBgmhivGQYGBgYGBgYGBgYGBgYGBgbmQw+P/eMrC5UTVAAAAABJRU5ErkJggg==',
+      }}
+    />
+    
+  );
+}
+
+export default function App() {
   const { t, i18n } = useTranslation();
-      return (
-<NavigationContainer>
-<Text style={{ fontFamily: 'Yekan' }}>{t('visits.UNUSUAL_ACTIVITY')}</Text>
-        <Drawer.Navigator initialRouteName="Home">
+  return (
+    <NavigationContainer>
 
-        
-          <Drawer.Screen name="Home" component={Home} />
-          <Drawer.Screen name="About" component={About} />
-        </Drawer.Navigator>
-      </NavigationContainer>
+      <Text style={{ fontFamily: 'Yekan' }}>{t('visits.UNUSUAL_ACTIVITY')}</Text>
+      <LogoTitle></LogoTitle>
+     
+  
+      <Drawer.Navigator initialRouteName="Home" screenOptions={{ headerTitleStyle: { color: '#fff' }}} drawerPosition="left" drawerType="slide" drawerContent={(props) => <CustomDrawerContent {...props} />}>
+        <Drawer.Screen name="Home" component={Home} options={{ headerBa: props => <LogoTitle {...props} /> }} />
+        <Drawer.Screen name="About" component={About} Icon={{ uri: 'https://avatars0.githubusercontent.com/u/17571969?v=3&s=400' }} options={{ headerTitleStyle: { color: '#fff' }, }}/>
 
-
-
-
-        // const Tab = createBottomTabNavigator();
-        // <NavigationContainer>
-        // <Tab.Navigator>
-        //   <Tab.Screen name="First">
-        //     {() => (
-        //       <SettingsStack.Navigator>
-        //         <SettingsStack.Screen
-        //           name="Settings"
-        //           component={Home}
-        //         />
-        //         <SettingsStack.Screen name="Home" component={Home} />
-        //       </SettingsStack.Navigator>
-        //     )}
-        //   </Tab.Screen>
-        //   <Tab.Screen name="Second">
-        //     {() => (
-        //       <HomeStack.Navigator>
-        //         <HomeStack.Screen name="Home" component={Home} />
-        //         <HomeStack.Screen name="About" component={About} />
-        //       </HomeStack.Navigator>
-        //     )}
-        //   </Tab.Screen>
-        // </Tab.Navigator>
-      // </NavigationContainer>
-
-//        const Stack = createStackNavigator();
-//         <NavigationContainer>
-//         <Stack.Navigator initialRouteName="Home">
-//         <Stack.Screen name="Home" component={Home} />
-//           {/* <Stack.Screen name="Home">
-//           {props => <Home {...Stack}/>}
-//           </Stack.Screen> */}
-
-
-//           {/* component={Home} */}
-//           {/* <Stack.Screen name="Home">
-//   {props => <HomeScreen {...props} extraData={someData} />}
-// </Stack.Screen> */}
-//           <Stack.Screen name="About" component={About} />
-//         </Stack.Navigator>
-//       </NavigationContainer>
-      );
-  }
-
-  // <Router>
-  //       <View style={styles.container}>
-  //         <View style={styles.nav}>
-  //         <Link
-  //       to={`${match.url}/rendering`}
-  //       style={styles.subNavItem}
-  //       underlayColor="#f0f4f7"
-  //     >
-  //       <Text>Rendering with React</Text>
-  //     </Link>
-  //           <Link to="/">
-  //             <Text>Home</Text>
-  //           </Link>
-  //           <Link to="/about">
-  //             <Text>About</Text>
-  //           </Link>
-  //         </View>
-    
-  //         <Route exact path="/" component={Home} />
-  //         <Route path="/about" component={About} />
-  //       </View>
-  //     </Router>
-
-// const App = () => (
-// <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        //   <Text style={{ fontSize: 20, marginBottom: 20 }}>{t('hello')}</Text>
-    
-        //   <TouchableOpacity onPress={() => i18n.changeLanguage(i18n.language === 'fa' ? 'en' : 'fa')}>
-        //     <Text style={{ fontFamily: 'Yekan' }}>{t('visits.UNUSUAL_ACTIVITY')}</Text>
-        //   </TouchableOpacity>
-        // </View>
-// );
+        {/* options={{ headerTitle: props => <LogoTitle {...props} /> }}  options={{ title: 'My home' }}*/}
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
     marginTop: 25,
     padding: 10
   },
-  nav:{
+  nav: {
     flexDirection: 'row',
     justifyContent: 'space-around',
   },
